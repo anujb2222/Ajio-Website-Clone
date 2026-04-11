a84a1e5c6c8060e83ac61c73ddfa09ccfe52d81c
anujb@1234 <anujb2075@gmail.com>
2026-04-11 22:38:38 +0530

    comit

--- ajio-backend/server.js
+++ ajio-backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // IMPORTANT FIX
+app.use(express.urlencoded({ extended: true })); 
 
 // ================== DB ==================
 mongoose.connect(process.env.MONGO_URL)
