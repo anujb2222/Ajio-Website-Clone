2a3fc54c281d228e6078b736aed2e4ef1643adb8
anujb@1234 <anujb2075@gmail.com>
2026-04-12 19:51:49 +0530

    chages

--- ajio-backend/server.js
+++ ajio-backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("dns").setDefaultResultOrder("ipv4first");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
