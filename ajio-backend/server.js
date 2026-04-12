7c01eb56730697de2f1155d093cb646c61e8d585
anujb@1234 <anujb2075@gmail.com>
2026-04-12 20:22:34 +0530

    changes

--- ajio-backend/server.js
+++ ajio-backend/server.js
@@ -74,16 +74,14 @@ app.post("/send-otp", async (req, res) => {
 );
 
  const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
-
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: email,
