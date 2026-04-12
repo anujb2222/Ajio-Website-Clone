f74228dc4ce735d3953939f99e81e13fdf29f099
anujb@1234 <anujb2075@gmail.com>
2026-04-12 20:08:17 +0530

    chanes

--- ajio-backend/server.js
+++ ajio-backend/server.js
@@ -74,9 +74,7 @@ app.post("/send-otp", async (req, res) => {
 );
 
  const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
