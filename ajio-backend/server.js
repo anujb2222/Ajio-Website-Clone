5cd6efa84209cb14841f45d4219cf72571fc4fd4
anujb@1234 <anujb2075@gmail.com>
2026-04-13 10:49:22 +0530

    changes

--- ajio-backend/server.js
+++ ajio-backend/server.js
@@ -21,4 +21,6 @@ app.use("/orders", orderRoutes);
 
 connectDB();
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
