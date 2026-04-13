3888d5121f41a68292b6cbead201db1288b4a3ef
anujb@1234 <anujb2075@gmail.com>
2026-04-13 19:30:05 +0530

    changes

--- ajio-backend/routes/productRoutes.js
+++ ajio-backend/routes/productRoutes.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const multer = require("multer");
 const productController = require("../controllers/productController");
 
-const storage = multer.memoryStorage();
+const storage = multer.memoryStorage(); // ✅ MUST be memory
 const upload = multer({ storage });
 
 router.post("/additem", upload.single("image"), productController.addItem);
 router.put("/updateitem/:id", upload.single("image"), productController.updateItem);
-
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 router.delete("/:id", productController.deleteProduct);
