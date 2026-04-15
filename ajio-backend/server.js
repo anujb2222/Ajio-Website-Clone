f3a669d47c79791587296f62fbf10a53f8271989
anujb@1234 <anujb2075@gmail.com>
2026-04-15 20:56:34 +0530

    changes

--- ajio-backend/server.js
+++ ajio-backend/server.js
@@ -9,8 +9,11 @@ const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 
-
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
